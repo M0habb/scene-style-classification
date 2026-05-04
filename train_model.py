0596fa68ac994ec3cff6e7613955613fe5cd84ad
SarahHaimy <saso.thomas2006@gmail.com>
2026-05-04 16:48:04 +0300

    l

--- train_model.py
+++ train_model.py
@@ -2,6 +2,7 @@ from models.vgg import VGG16
 from utils.dataset import get_loaders
 from train import train
 import torch
+from models import cnn
 from config import DEVICE, LEARNING_RATE, VIT_LEARNING_RATE
 import gc
 from evaluate import evaluate_all
@@ -20,34 +21,35 @@ torch.cuda.empty_cache()
 _, _, class_names, num_classes = get_loaders()
 
 # Instantiate model
-vgg_model = VGG16(num_classes=num_classes)
+#vgg_model = VGG16(num_classes=num_classes)
+cnn_model = cnn()
 
 # LOADING PRETRAINED WEIGHTS
-PRETRAINED_PATH = "pretrained/vgg16_bn.pth"
+# PRETRAINED_PATH = "pretrained/vgg16_bn.pth"
 
-# Load the pretrained state dict
-pretrained_dict = torch.load(PRETRAINED_PATH, map_location=DEVICE)
+# # Load the pretrained state dict
+# pretrained_dict = torch.load(PRETRAINED_PATH, map_location=DEVICE)
 
-# Get the model's own state dict
-model_dict = vgg_model.state_dict()
-pretrained_dict = {
-    k: v for k, v in pretrained_dict.items()
-    if k in model_dict and model_dict[k].shape == v.shape
-}
+# # Get the model's own state dict
+# model_dict = vgg_model.state_dict()
+# pretrained_dict = {
+#     k: v for k, v in pretrained_dict.items()
+#     if k in model_dict and model_dict[k].shape == v.shape
+# }
 
-# Update model dict with pretrained weights
-model_dict.update(pretrained_dict)
-vgg_model.load_state_dict(model_dict)
+# # Update model dict with pretrained weights
+# model_dict.update(pretrained_dict)
+# vgg_model.load_state_dict(model_dict)
 
-print(f"✅ Pretrained weights loaded — {len(pretrained_dict)} layers transferred")
-print(f"   Classifier layers kept random (shape mismatch with ImageNet 1000 classes)")
+# print(f"✅ Pretrained weights loaded — {len(pretrained_dict)} layers transferred")
+# print(f"   Classifier layers kept random (shape mismatch with ImageNet 1000 classes)")
 
 
 # Train
 best_acc, save_path = train(
-    model      = vgg_model,
-    model_name = "vgg_pretrained",
-    lr         = VIT_LEARNING_RATE,       # lower LR when using pretrained weights
+    model      = cnn_model,
+    model_name = "cnn_v1",
+    lr         = LEARNING_RATE,       # lower LR when using pretrained weights
 )
 
 # best_acc, save_path = train(
@@ -61,14 +63,14 @@ best_acc, save_path = train(
 model_registry = {
     "vgg_pretrained": VGG16,
     # "vgg":    VGG16,
-    # "cnn":    CustomCNN,
+    "cnn":    cnn,
     # "resnet": CustomResNet,
     # "vit":    CustomViT,
 }
 
-all_results = evaluate_all(
-    model_registry=model_registry,
-    num_classes=num_classes,
-    class_names=class_names,
-    save_dir="models"
-)
+#all_results = evaluate_all(
+#    model_registry=model_registry,
+#    num_classes=num_classes,
+#    class_names=class_names,
+#    save_dir="models"
+#)
